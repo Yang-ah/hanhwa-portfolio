c2ceebf2db60cc5fa871b32bea4a62d06726157c
Yang-ah <yangah0409@gmail.com>
2022-12-02 00:51:44 +0900

    add subGnb

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@
 const btn = document.querySelector("#subGnbBtn");
 const btnI = document.querySelector("#subGnbBtn~label");
 const subGnb = document.querySelector(".subGnb");
+const Xbtn = document.querySelector(".subGnb-Xbtn");
+const subLi = document.querySelector(".subGnb-nav");
+
+const showList = () => {};
+const removeSub = () => {
+  subGnb.classList.add("hidden");
+  onHidden();
+};
 
 const onClick = () => {
   btn.classList.toggle("hidden");
@@ -29,3 +37,5 @@ const btnChk = () => {
 window.onload = onHidden();
 btn.addEventListener("click", onClick);
 window.addEventListener("resize", onHidden);
+Xbtn.addEventListener("click", removeSub);
+subLi.addEventListener("click", showList);
