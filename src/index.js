bd8017daa506a1f6cb549c5e1020d99f1247ffec
Yang-ah <yangah0409@gmail.com>
2022-12-05 19:02:17 +0900

    done with the tablet js, style

--- src/index.js
+++ src/index.js
@@ -3,12 +3,47 @@ const btn = document.querySelector("#subGnbBtn");
 const btnI = document.querySelector("#subGnbBtn~label");
 const subGnb = document.querySelector(".subGnb");
 const Xbtn = document.querySelector(".subGnb-Xbtn");
-const subLi = document.querySelector(".subGnb-nav");
+const showListBtn = document.querySelectorAll(".showListBtn");
+const showListBtnI = document.querySelectorAll(".showListBtn i");
+const subList = document.querySelectorAll(".subList");
+const subGnbnav = document.querySelectorAll(".subGnb-nav");
 
-const showList = () => {};
+const upIcon = "fas fa-chevron-up";
+const downIcon = "fas fa-chevron-down";
+
 const removeSub = () => {
   subGnb.classList.add("hidden");
   onHidden();
+};
+
+function listToggle(hl) {
+  let dataList = hl.target.dataset.list;
+
+  subList.forEach((subList) => {
+    if (
+      subList.dataset.list == dataList &&
+      subList.classList.contains("hidden")
+    ) {
+      subList.classList.remove("hidden");
+      changeI(dataList, "up");
+    } else if (
+      subList.dataset.list == dataList &&
+      !subList.classList.contains("hidden")
+    ) {
+      subList.classList.add("hidden");
+      changeI(dataList, "down");
+    }
+  });
+}
+
+const changeI = (dataList, status) => {
+  showListBtnI.forEach((icon) => {
+    if (icon.dataset.list == dataList && status == "up") {
+      icon.classList.value = upIcon;
+    } else if (icon.dataset.list == dataList && status == "down") {
+      icon.classList.value = downIcon;
+    }
+  });
 };
 
 const onClick = () => {
@@ -38,4 +73,7 @@ window.onload = onHidden();
 btn.addEventListener("click", onClick);
 window.addEventListener("resize", onHidden);
 Xbtn.addEventListener("click", removeSub);
-subLi.addEventListener("click", showList);
+
+subGnbnav.forEach((nav) => {
+  nav.addEventListener("click", listToggle);
+});
